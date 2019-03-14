9e3450454a0d6d288526ce193aa3f617262781b5
JaredRosenfeld <jpstarcatcher@gmail.com>
2019-03-14 17:40:23 +0200

    initial commit.

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 17:29:51 2019
+
+@author: jpsta
+"""
+
+print("Hello")
---
+++ hello_you.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 17:31:07 2019
+
+@author: jpsta
+"""
+
+print("The weather is beautiful today")
