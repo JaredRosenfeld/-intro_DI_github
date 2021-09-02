2cacf1748011a93fc8fba34882d940856b5a8c4d
JaredRosenfeld <43641883+JaredRosenfeld@users.noreply.github.com>
2021-09-02 18:56:10 +0300

    Update rosenfelddailychallenge day 2.py

--- rosenfelddailychallenge day 2.py
+++ rosenfelddailychallenge day 2.py
@@ -17,34 +17,23 @@ print(sum)
 
 
 birthday = input("Please enter your birthday with the following format DD/MM/YYYY: ")
+
 day_of_bith = birthday[0:2]
 month_of_birth = birthday[3:5]
 year_of_birth = birthday[-4:]
 birthday_final = date(year = int(year_of_birth), month = int(month_of_birth), day = int(day_of_bith))
 today = date.today()
+age = round((today-birthday_final).days/365.25)
 
-number_of_candels = int(year_of_birth[-1])
-print(number_of_candels)
-print(round((today-birthday_final).days/365.25))
+number_of_candels = age % 10
+candels = "i" * number_of_candels
 
-# t=number_of_candels
-# print(t)
-# d=4*'-'
-# s='  '
-# a='+\n'
-# r=(t-1)*s
-# print(r+' _|_|_\n'+r+'|     |')
-# for i in range(2,t+1):
-#     b=(t-i)*s;
-#     print(b+'+-'+i*d+a+b+'| '+i*2*s+'|')
-# print('+-'+t*d+a)
-
-
-print("___iiiii___")
-print("|:H:a:p:p:y:|")
-print("__|___________|__")
-print("|^^^^^^^^^^^^^^^^^|")
-print("|:B:i:r:t:h:d:a:y:|")
-print("|                 |")
-print("~~~~~~~~~~~~~~~~~~~")
+print(f"Congratulations you are {age} years old")
+print(f"      ___{candels}___")
+print("     |:H:a:p:p:y:|")
+print("   __|___________|__")
+print("  |^^^^^^^^^^^^^^^^^|")
+print("  |:B:i:r:t:h:d:a:y:|")
+print("  |                 |")
+print("  ~~~~~~~~~~~~~~~~~~~")
 
